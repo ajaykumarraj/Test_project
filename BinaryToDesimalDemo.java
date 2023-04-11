7f9e2915e55adbbdc0f78e4a7a8c7cc5de6a4865
Ajay Kumar (IN83003) <m1083003@mindtree.com>
2023-04-11 13:55:36 +0530

    First commit

---
+++ BinaryToDesimalDemo.java
@@ -0,0 +1,11 @@
+package apna.college;
+
+public class BinaryToDesimalDemo {
+
+	public static void main(String[] args) {
+		String num ="111111111111";
+		int desimalNo =Integer.parseInt(num, 2);
+		System.out.println(desimalNo);
+	}
+
+}
